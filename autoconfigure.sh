4b58baa5416e3f69c2264826c69e24d5b78f199d
SGC1990 <19717561+SGC1990@users.noreply.github.com>
2022-08-26 21:18:42 -0400

    add pfSense Plus 22.01

--- autoconfigure.sh
+++ autoconfigure.sh
@@ -9,3 +9,8 @@ if [ "$(cat /etc/version | cut -c 1-3)" == "2.5" ]; then
 	pkg install -y py38-speedtest-cli-2.1.3 
 	fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/SGC1990/pfsense-speedtest-widget/master/speedtest.widget.php
 fi
+
+if [ "$(cat /etc/version | cut -c 1-3)" == "22.01" ]; then
+	pkg install -y py38-speedtest-cli-2.1.3 
+	fetch -q -o /usr/local/www/widgets/widgets/speedtest.widget.php https://raw.githubusercontent.com/SGC1990/pfsense-speedtest-widget/master/speedtest.widget.php
+fi
